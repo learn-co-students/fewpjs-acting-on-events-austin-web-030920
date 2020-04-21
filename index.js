3bbd29d152165bcc684643a5105c50d8e4057d6f
Josh-Gotro <joshuagauthreaux@gmail.com>
2020-04-21 13:44:34 -0500

    Done.

--- index.js
+++ index.js
@@ -7,14 +7,18 @@ function moveDodgerLeft() {
     let left = parseInt(leftNumbers, 10);
    
     if (left > 0) {
-      dodger.style.left = `${left - 1}px`;
+      dodger.style.left = `${left - 10}px`;
     }
   }
 
   function moveDodgerRight() {
     let leftNumbers = dodger.style.left.replace("px", "");
     let left = parseInt(leftNumbers, 10);
-    dodger.style.left = `${left + 1}px`;
+    
+
+    if (left < 360) {
+        dodger.style.left = `${left + 10}px`;
+      }
   }
 
 
