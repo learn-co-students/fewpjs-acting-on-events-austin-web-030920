6f144306f44f79c8312962cb6bd76a902f63023b
iquery-001 <kota@vaga.tk>
2020-04-21 16:44:32 -0500

    Done.

--- .history/index_20200421162937.js
+++ .history/index_20200421162937.js
@@ -10,11 +10,11 @@ function moveDodgerLeft() {
 }
 
 function moveDodgerRight() {
-    let leftNumbers = dodger.style.left.replace("px", "");
-    let left = parseInt(leftNumbers, 10);
+    let rightNumbers = dodger.style.right.replace("px", "");
+    let right = parseInt(rightNumbers, 10);
 
-    if (left < 360) {
-        dodger.style.left = `${left + 1}px`;
+    if (right > 0) {
+        dodger.style.left = `${left 1}px`;
     }
 }
 
